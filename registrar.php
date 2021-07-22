f84ae44e8fca1e24e91063e739a3d4bd7bf34273
Beatriz Pereira <64531513+Beatriz-Pereira@users.noreply.github.com>
2021-07-22 19:54:57 -0300

    Add files via upload
    Scripts iniciais

---
+++ registrar.php
@@ -0,0 +1,20 @@
+<?php
+    include_once 'conexao.php';
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $nome = $_POST['nome'];
+
+    $sql2 = $dbcon->query("SELECT * FROM tblogin WHERE  email = '$email'");
+
+    if(mysqli_num_rows($sql2)>0){
+        echo "email_erro";
+    }else {
+        $sql3 = $dbcon->query("INSERT INTO tblogin(nome,email,senha) values ('$nome','$email','$senha')");
+        
+        if ($sql3) {
+            echo 'registro_ok';
+        }else {
+            echo 'registro_erro';
+        }
+    }
+?>
